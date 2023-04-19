10489b46a4be1f14508e5800c9502f059a3fb571
dsprog <dsprog@gmail.com>
2023-04-19 15:56:48 -0300

    iniciando configuração de marcas

--- app/Http/Controllers/Api/V1/MarcaController.php
+++ app/Http/Controllers/Api/V1/MarcaController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 
@@ -9,17 +10,19 @@ class MarcaController extends Controller
 {
     public function index()
     {
-        //
+        $marcas = Marca::all();
+        return $marcas;
     }
 
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all());
+        return $marca;
     }
 
     public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
     
     public function update(Request $request, Marca $marca)
@@ -29,6 +32,7 @@ class MarcaController extends Controller
 
     public function destroy(Marca $marca)
     {
-        //
+        $marca = $marca->delete();
+        return $marca;
     }
 }
